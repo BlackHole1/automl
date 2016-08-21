01b7ced89d043b149f01bcdc6cd4846dca03ca53
Black-Hole <black-hole@localhost.localdomain>
2016-08-22 01:11:55 +0800

    上次初始化构架时上传了多余的目录

--- controllers/site_controller.js
+++ controllers/site_controller.js
@@ -6,6 +6,6 @@ controller.prototype.index_action = function(){
     this.res.send('i am actionIndex');
 };
 controller.prototype.about_action = function(){
-    this.res.send(['BOSS系统','ver:0.01','author:BYTS','date:2014-1-1']);
+    this.res.send('test');
 };
 exports.controller = controller;
