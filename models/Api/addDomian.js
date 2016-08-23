7b89a0b5d40b9758702ecd03dc774d311b99d368
Black-Hole <black-hole@localhost.localdomain>
2016-08-23 23:47:28 +0800

    删除一些测试文件

--- models/Api/addDomian.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/automl');
-var PersonSchema = new mongoose.Schema({
-      url:String
-});
-module.exports = mongoose.model('domian_list', PersonSchema,'domian_list');
